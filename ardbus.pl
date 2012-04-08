4f180ada88d59fbe518e2e5cb6e415cb4c870975
Vitaly _Vi Shukela <vi0oss@gmail.com>
2012-04-09 02:36:09 +0300

    Implement DigitalInputChanged signal in service

--- ardbus.pl
+++ ardbus.pl
@@ -30,6 +30,7 @@ sub new {
 
 dbus_method("DigitalWrite", ["byte", "bool"]);
 dbus_method("PinMode", ["byte", "bool"]);
+dbus_signal("DigitalInputChanged", ["byte", "bool"]);
 
 sub DigitalWrite($$) {
     my $self = shift;
@@ -53,6 +54,23 @@ sub PinMode($$) {
     print "ArMo".$pinModeValues."\n";
 }
 
+sub digitalInputFromArduino($$) {
+    my $self = shift;
+    my $string = shift;
+
+    my ($i,$j);
+
+    return unless length($string) == 4+$lpc;
+
+    for($i=0, $j=4; $i<$lpc; ++$i, ++$j) {
+        my $n = substr($string,            $j, 1);
+        my $o = substr($digitalReadValues, $i, 1);
+        unless($n eq $o) {
+            substr($digitalReadValues, $i, 1) = $n;
+            $self->emit_signal("DigitalInputChanged", $i+$MASKED_PINS, not($n eq "0"));
+        }
+    }
+}
 
 
 package main;
@@ -69,13 +87,13 @@ $|=1;
 
 my $bus = Net::DBus->session;
 my $service = $bus->export_service("org.vi_server.ardbus");
-my $object = MyObj->new($service);
+our $object = MyObj->new($service);
 my $reactor = Net::DBus::Reactor->main();
 
 $reactor->add_read(fileno(F), Net::DBus::Callback->new(method => sub { 
     my $readie="";
     sysread F, $readie, 40;
-    #printf STDOUT $readie;
+    $object->digitalInputFromArduino($1) if $readie =~ /(ArDi[01]+)\n/;
 }, args => []));
 
 $reactor->run();
